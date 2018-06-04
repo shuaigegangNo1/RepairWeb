85eaf4713cdcebfc45b5c7e7023ff0322b387c1a
黄雪雯 <huangxuewen@huangxuewen.local>
2018-06-04 22:22:09 +0800

    modify area value

--- src/app/common/pipe/area.pipe.ts
+++ src/app/common/pipe/area.pipe.ts
@@ -9,7 +9,7 @@ import {Pipe, PipeTransform} from '@angular/core';
 export class AreaPipe implements PipeTransform {
 
     transform(value: number, args?: any): any {
-        if (value === 0) {
+        if (value === 10) {
             return '公寓区'; // 物业
         } else if (value === 11) {
             return '图文楼';
@@ -23,11 +23,11 @@ export class AreaPipe implements PipeTransform {
             return '专业教学楼';
         } else if (value === 16) {
             return '文体中心'; // 曹云峰
-        } else if (value === 2) {
+        } else if (value === 20) {
             return '室外公共设施'; // 徐雪荣
-        } else if (value === 3) {
-            return '绿化卫生'; // 叶琳
-        } else if (value === 4) {
+        } else if (value === 30) {
+            return '绿化卫生'; // 叶琳、于茂华
+        } else if (value === 40) {
             return '食堂'; // 叶翠云
         } else if (value === 17) {
             return '其他';
